06033622bcbecd8fd18a75976db9eb07e1d79548
Lucas da Luz Morais <llucaslluz@gmail.com>
2025-12-09 20:12:09 -0300

    adicionando outros itens nas listas

--- bin/email.dart
+++ bin/email.dart
@@ -6,6 +6,8 @@ void enviarEmail(String email){
 void main() {
   List<String> listaEmails = ["joao@gmail.com","maria@gmail.com"];
 
+  listaEmails.add("enzo@gmail.com");
+
   for(String email in listaEmails){
     enviarEmail(email);
   }
