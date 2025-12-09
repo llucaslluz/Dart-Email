239db48d37af22e8f454560377e4d092d5dec614
Lucas da Luz Morais <llucaslluz@gmail.com>
2025-12-09 20:20:53 -0300

    testando a função SET

--- bin/email.dart
+++ bin/email.dart
@@ -4,16 +4,24 @@ void enviarEmail(String email){
 }
 
 void main() {
-  List<String> listaEmails = ["joao@gmail.com","maria@gmail.com"];
+  // List<String> listaEmails = ["joao@gmail.com","maria@gmail.com"];
 
-  listaEmails.add("enzo@gmail.com");
+  // listaEmails.add("enzo@gmail.com");
 
-  if(listaEmails.contains("enzo@gmail.com")==false){
-    listaEmails.add("enzo@gmail.com");
-  }
+  // if(listaEmails.contains("enzo@gmail.com")==false){
+  //   listaEmails.add("enzo@gmail.com");
+  // }
 
-  for(String email in listaEmails){
-    enviarEmail(email);
+  // for(String email in listaEmails){
+  //   enviarEmail(email);
+  // }
+
+  Set<String> setEmails = {"joao@gmail.com","maria@gmail.com"};
+
+  setEmails.add("enzo@gmail.com");
+
+  for (String email in setEmails){
+    print(email);
   }
 
 }
